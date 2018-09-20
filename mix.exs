eb48cdd9a0e338fcdcd1a3bc2a63e938271f898d
gentom <gento.m.developer@gmail.com>
2018-09-20 23:49:56 +0900

    add escript

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Exclidemo.MixProject do
   def project do
     [
       app: :exclidemo,
+      escript: [main_module: Exclidemo.CLI],
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
